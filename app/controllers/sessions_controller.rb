bf2d4893e8be87132e3a8fa0592e2e6be88d10e8
yu3630 <you19910630@gmail.com>
2021-02-21 00:17:04 +0000

    rails 05 Task

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,9 +12,23 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+  def destroy
+    log_out
+    redirect_to root_url, info: "ログアウトしました"
+  end
+
 
   private
   def log_in(user)
     session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.dalete(:user_id)
+    @current_user = nil
+  end
+  
+  def session_params
+    params.require(:session).permit(:email,:password)
   end
 end
