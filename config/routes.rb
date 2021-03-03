b9da2e8af2f560c7c64f9e84cded67b25818fdee
yu3630 <you19910630@gmail.com>
2021-03-03 02:32:07 +0000

    07 Task

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,17 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
   
-  resources :users
-  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+ 
+  resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+  
+  get 'comments/new', as: 'comments'
+  post '/comments', to: 'comments#create', as: 'comments_create'
 end
